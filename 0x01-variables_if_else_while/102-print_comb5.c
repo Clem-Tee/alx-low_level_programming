0dc5366e6edf36ddd5f0d7e0b63aa746d0ec6abe
Clem-Tee <clementogol@gmail.com>
2021-08-27 20:33:40 +0300

    A C program that prints all possible combinations of two two-digit numbers, ranging from 0-99, separated by a comma followed by a space

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,15 +1,15 @@
 /*
  * File: 102-print_comb5.c
- * Auth: Clement Ogol
+ * Auth: Clement Ogol 
  */
 
 #include <stdio.h>
 
 /**
  * main - Prints all possible combinations of two two-digit numbers,
- *	  ranging from 0-99, separated by a comma followed by a space
- * 
- * Return: Always 0
+ *        ranging from 0-99, separated by a comma followed by a space.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
@@ -17,7 +17,7 @@ int main(void)
 
 	for (num1 = 0; num1 <= 98; num1++)
 	{
-		for (num2 = num1 +1; num2 <= 99; num2++)
+		for (num2 = num1 + 1; num2 <= 99; num2++)
 		{
 			putchar((num1 / 10) + '0');
 			putchar((num1 % 10) + '0');
