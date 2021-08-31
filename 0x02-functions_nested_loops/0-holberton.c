d82395111821de011942dbf501f2d2d75718fb7d
Clem-Tee <clementogol@gmail.com>
2021-08-31 08:47:36 +0300

    Prints Holberton by using oly user defined variables

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,25 @@
+#include "notrebloh.h"
+
+/**
+ * main - Entry Point.use only defined variable to create output.
+ *
+ * Description: Print notrebloh using predefined functions
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchur('\n');
+
+	return (0);
+}
