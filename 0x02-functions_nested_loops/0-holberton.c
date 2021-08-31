07b78ccf6347e08eb86ac814d205c4df12c9db4e
Clem-Tee <clementogol@gmail.com>
2021-08-31 18:47:45 +0300

    A function that prints the alphabet, in lowercase, followed by a new line

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,25 +1,20 @@
 #include "main.h"
-
 /**
- * main - Entry Point.use only defined variable to create output.
- *
- * Description: Print notrebloh using predefined functions
- *
+ * main - Print "Holberton" followed by a new line.
+ * Description: You are not allowed to include standard libraries.
  * Return: 0
  */
-
 int main(void)
 {
-	char h[] = "Holberton";
-	int m;
+	char holberton[] = "Holberton";
+	int i = 0;
 
-	m = 0;
-
-	while (m <= 8)
+	while (holberton[i] != '\0')
 	{
-		_putchar(h[m]);
-		m++;
+		_putchar(holberton[i]);
+		i++;
 	}
 	_putchar('\n');
+
 	return (0);
 }
