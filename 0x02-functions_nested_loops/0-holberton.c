40fa2497b04edba64fbe2bbf16a537981ec3377e
Clem-Tee <clementogol@gmail.com>
2021-08-31 18:44:14 +0300

    A function that prints the alphabet, in lowercase, followed by a new line

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -10,16 +10,16 @@
 
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
-	_putchar('t');
-	_putchar('o');
-	_putchar('n');
-	_putchar('\n');
+	char h[] = "Holberton";
+	int m;
 
+	m = 0;
+
+	while (m <= 8)
+	{
+		_putchar(h[m]);
+		m++;
+	}
+	_putchar('\n');
 	return (0);
 }
