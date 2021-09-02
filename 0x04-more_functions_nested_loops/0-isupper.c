eb8e0ca678de64805cb0b543024e0a6be5e586f3
Clem-Tee <clementogol@gmail.com>
2021-09-02 13:46:31 +0300

    A program that checks for uppercase character

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,18 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _isupper - checks if input is an uppercase letter in ASCII
- * @c: integer to check
- * Return: 1 if true, 0 if false
- */
-int _isupper(int c)
+   * main - check the code for Holberton School students.
+    *
+     * Return: Always 0.
+      */
+int main(void)
 {
-	return (c >= 65 && c <= 90);
+	    char c;
+
+	        c = 'A';
+		    printf("%c: %d\n", c, _isupper(c));
+		        c = 'a';
+			    printf("%c: %d\n", c, _isupper(c));
+			        return (0);
 }
