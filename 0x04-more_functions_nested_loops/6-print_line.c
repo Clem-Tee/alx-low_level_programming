8314b6b1a05262c535cc9e55562c81e8a33c8c22
Clem-Tee <clementogol@gmail.com>
2021-09-02 18:45:30 +0300

    A program that draws a straight line in the terminal

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,15 +1,26 @@
-#include <mai.h>
+#include "main.h"
 
 /**
- * print_line - print line of variable length
- * @n: lenth of line
- */
+  * print_line - Draws a straight line according to parameter
+  * @n: The number of lines to draw
+  *
+  * Return: empty
+  */
 void print_line(int n)
 {
-	int i;
+	int x;
 
-	if (n > 0)
-		for (i = 0; i < n; i++)
-			_putchar('_');
-	_putchar('\n');
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x < n; x++)
+		{
+			_putchar(95);
+		}
+
+		_putchar('\n');
+	}
 }
