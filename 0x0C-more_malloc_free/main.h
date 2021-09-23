f6459d8b2b714796b9f71ef3638cbaf1a94fecd2
Clem-Tee <clementogol@gmail.com>
2021-09-24 02:21:32 +0300

    Added a header file for the prototypes

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -27,5 +27,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int onlyNumbers(char *c);
+void multiply(char* s1, char* s2);
 
 #endif /* _MAIN_H_ */
