cb2bcadbc2927d2423dfe4206f28b228b1bd5107
Clem-Tee <clementogol@gmail.com>
2021-10-12 23:12:17 +0300

    Added a function that inserts a new node at a given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,8 +1,8 @@
-#include <stdlib.h>
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * insert_nodeint_at_index - inserts a new node at a given position
+ * insert_nodeint_at_index -  inserts a new node at a given position.
  * @head: double pointer to the first node in the list
  * @idx: index of the node to insert
  * @n: value for new node
