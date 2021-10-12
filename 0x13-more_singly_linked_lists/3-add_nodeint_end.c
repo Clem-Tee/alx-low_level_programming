d9949b6ad66bd0e9dc1fa853337ce51dca44e7ec
Clem-Tee <clementogol@gmail.com>
2021-10-12 23:06:18 +0300

    Added a function that adds a new node at the end of a list

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,9 +1,10 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
 
 /**
  * add_nodeint_end - adds a new node at the end of a listint_t list
  * @head: double pointer to the beginning of the list
+ * @n: integer to add to the list
  *
  * Return: pointer to the new node
  */
