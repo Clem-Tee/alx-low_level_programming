27b025c3e86c626e4f7a51537a34eb75675a7e34
Clem-Tee <clementogol@gmail.com>
2023-05-24 15:27:33 +0300

    Program to multiply two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,128 +1,41 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 
-/**
- * main - multiplies two positive numbers
- * @argc: argument count
- * @argv: argument vectors
- * Return: 0
- */
-int main(int argc, char *argv[])
-{
-	char *f = argv[1];
-	char *s = argv[2];
-
-	if (argc != 3 || !onlyNumbers(f) || !onlyNumbers(s))
-	{
-		printf("Error\n");
-		exit(98);
-	}
-	if (*f == 48 || *s == 48)
-		printf("0\n");
-	else
-		multiply(s, f);
-	return (0);
-}
-
-/**
- * multiply - multiplies two numbers and displays it
- * @f: first "number"
- * @s: second "number"
- */
-void multiply(char *f, char *s)
-{
-	int i, len1, len2, total, fdigit, sdigit, res = 0, tmp;
-	int *ptr;
-
-	len1 = _strlen(f);
-	len2 = _strlen(s);
-	tmp = len2;
-	total = len1 + len2;
-	ptr = _calloc(sizeof(int), (len1 + len2));
-	for (len1--; len1 >= 0; len1--)
-	{
-		fdigit = f[len1] - '0';
-		res = 0;
-		len2 = tmp;
-		for (len2--; len2 >= 0; len2--)
-		{
-			sdigit = s[len2] - '0';
-			res += ptr[len2 + len1 + 1] + (fdigit * sdigit);
-			ptr[len1 + len2 + 1] = res % 10;
-			res /= 10;
+// function to check if a string represents a positive number
+int is_positive_number(char *str) {
+	int i = 0;
+	while (str[i]) {
+		if (!isdigit(str[i])) {
+			return 0;
 		}
-		if (res)
-			ptr[len1 + len2 + 1] = res % 10;
+		i++;
 	}
-	while (*ptr == 0)
-	{
-		ptr++;
-		total--;
+	return 1;
+}
+
+int main(int argc, char *argv[]) {
+	// check if the correct number of arguments is provided
+	if (argc != 3) {
+		printf("Error\n");
+		return 98;
+	} 
+
+	// check if the arguments are positive numbers
+	if (!is_positive_number(argv[1]) || !is_positive_number(argv[2])) {
+		printf("Error\n");
+		return 98;
 	}
-	for (i = 0; i < total; i++)
-		printf("%i", ptr[i]);
-	printf("\n");
-}
-/**
- * onlyNumbers - determines if string has only numbers
- * @c: input string
- * Return: 0 if false, 1 if true
- */
-int onlyNumbers(char *c)
-{
-	while (*c)
-	{
-		if (*c < '0' || *c > '9')
-			return (0);
-		c++;
-	}
-	return (1);
-}
 
-/**
- * _strlen - returns the length of a string
- * @s: string s
- * Return: length of string
- */
-int _strlen(char *s)
-{
-	char *p = s;
+	// convert the arguments to integers
+	int num1 = atoi(argv[1]);
+	int num2 = atoi(argv[2]);
 
-	while (*s)
-		s++;
-	return (s - p);
-}
+	// perform the multiplication
+	int result = num1 * num2;
 
-/**
- * _memset - fills memory with a constant byte
- * @s: memory area
- * @b: constant byte
- * @n: bytes of the memory area
- * Return: pointer to the memory area s
- */
-char *_memset(char *s, char b, unsigned int n)
-{
-	char *ptr = s;
+	// print the result
+	printf("%d\n", result);
 
-	while (n--)
-		*s++ = b;
-	return (ptr);
-}
-
-/**
- * _calloc - allocates memory for an array, using malloc
- * @nmemb: number of elements of pointer
- * @size: size of each member
- * Return: pointer of allocated memory
- */
-void *_calloc(unsigned int nmemb, unsigned int size)
-{
-	void *ptr;
-
-	if (!nmemb || !size)
-		return (NULL);
-	ptr = malloc(size * nmemb);
-	if (!ptr)
-		return (NULL);
-	_memset(ptr, 0, size * nmemb);
-	return (ptr);
+	return 0;
 }
