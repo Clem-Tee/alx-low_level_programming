61ffde20f2a0d2c20595bf7a92e013a5ebf57a2d
Clem-Tee <clementogol@gmail.com>
2023-05-25 14:58:44 +0300

    Updated the main file

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,6 +1,16 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+
+#define min(x, y) (((x) < (y)) ? (x) : (y))
+
+int len(char *str);
+int find_len(char *str);
+char *create_xarray(int size);
+char *iterate_zeroes(char *str);
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -29,5 +39,9 @@ int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int onlyNumbers(char *c);
 void multiply(char* s1, char* s2);
+
+void get_prod(char *prod, char *mult, int digit, int zeroes);
+void add_nums(char *final_prod, char *next_prod, int next_len);
+
 
 #endif /* _MAIN_H_ */
